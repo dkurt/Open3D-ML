123e26835742d4695c6dd7b37b8f1accf503981c
Dmitry Kurtaev <dmitry.kurtaev@intel.com>
2021-09-27 17:25:54 +0300

    Remove extra class for Torch conversion

--- ml3d/torch/models/openvino_model.py
+++ ml3d/torch/models/openvino_model.py
@@ -8,23 +8,6 @@ import torch.nn as nn
 from openvino.inference_engine import IECore
 
 from .. import dataloaders
-from .kpconv import KPFCNN
-
-
-class KPFCNN_OV(nn.Module):
-
-    def __init__(self, base_model):
-        super().__init__()
-        self.base_model = base_model
-
-    def forward(self, inputs):
-        tensors = namedtuple('KPConvBatch', [])
-        tensors.features = inputs['features']
-        tensors.points = inputs['points']
-        tensors.neighbors = inputs['neighbors']
-        tensors.pools = inputs['pools']
-        tensors.upsamples = inputs['upsamples']
-        return self.base_model(tensors)
 
 
 class OpenVINOModel:
@@ -32,11 +15,7 @@ class OpenVINOModel:
     def __init__(self, base_model):
         self.ie = IECore()
         self.exec_net = None
-
-        if isinstance(base_model, KPFCNN):
-            self.base_model = KPFCNN_OV(base_model)
-        else:
-            self.base_model = base_model
+        self.base_model = base_model
 
         # A workaround for unsupported torch.square by ONNX
         torch.square = lambda x: torch.pow(x, 2)
@@ -63,18 +42,26 @@ class OpenVINOModel:
         return inputs
 
     def _read_torch_model(self, inputs):
-        input_names = self._get_input_names(inputs)
+        tensors = self._get_inputs(inputs)
+        input_names = self._get_input_names(tensors)
+
+        # Forward origin inputs instead of export <tensors>
+        origin_forward = self.base_model.forward
+        self.base_model.forward = lambda x: origin_forward(inputs)
 
         buf = io.BytesIO()
-        torch.onnx.export(self.base_model, inputs, buf, input_names=input_names)
-        # torch.onnx.export(self.base_model, inputs, 'kpconv.onnx', input_names=input_names)
+        torch.onnx.export(self.base_model, tensors, buf, input_names=input_names)
+        # torch.onnx.export(self.export_model, inputs, 'kpconv.onnx', input_names=input_names, opset_version=11)
+        self.base_model.forward = origin_forward
+
         net = self.ie.read_network(buf.getvalue(), b'', init_from_buffer=True)
         self.exec_net = self.ie.load_network(net, 'CPU')
 
     def forward(self, inputs):
-        inputs = self._get_inputs(inputs)
         if self.exec_net is None:
             self._read_torch_model(inputs)
+
+        inputs = self._get_inputs(inputs)
 
         tensors = {}
         for name, tensor in inputs.items():
